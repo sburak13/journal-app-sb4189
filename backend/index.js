3caf9b7e041785031d87e63dc34325dbdfa5edc0
Samantha Burak <samantha.burak@gmail.com>
2024-05-08 17:31:06 -0400

    Install packages and change index.js for railway

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const entryRoutes = require('./routes/entry-routes');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_DB_URI);
 
 app.use(cors());
@@ -19,6 +21,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'API works' });
 });
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
