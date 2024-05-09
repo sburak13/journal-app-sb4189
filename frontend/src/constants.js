3c9748546038c744f30fd169fab0e6a0ee54454b
Samantha Burak <samantha.burak@gmail.com>
2024-05-08 21:41:58 -0400

    Correctly format URI

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
 export const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-export const RAILWAY_DEPLOY_URI = "journal-app-sb4189-backend-production.up.railway.app"
+export const RAILWAY_DEPLOY_URI = "https://journal-app-sb4189-backend-production.up.railway.app"
